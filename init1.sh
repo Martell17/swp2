eed0c34ba3f71d006dea7f7f9a98dca61de250d7
Martell17 <martell17@mail.ru>
2016-11-23 23:08:42 +0900

    Update init1.sh

--- init1.sh
+++ init1.sh
@@ -1,3 +1,7 @@
 
+sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
+
+#sudo /etc/init.d/nginx restart
+sudo service nginx restart
+
