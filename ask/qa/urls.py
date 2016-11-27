01f69588eee6be563fc4b67fd8958b12b3bc9f22
martell17 <martell17@mail.ru>
2016-11-27 10:58:59 +0000

    first django 2

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,2 +1,9 @@
 urlpatterns =  patterns('qa.views',
-  url(r'^$', ),
+  url(r'^$', 'test', name = 'root'),
+  url(r'^login/', 'test'),
+  url(r'^signup/', 'test', name = 'signup'),
+  url(r'^question/\w+/', 'test', name = 'question'),
+  url(r'^ask/', 'test', name = 'ask'),
+  url(r'^popular/', 'test', name = 'popular'),
+  url(r'^new/', 'test', name = 'new'),
+)
