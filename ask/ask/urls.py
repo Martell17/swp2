75de0f81ecc88bf0dee7639b855258a3ee50f458
Pavel <martell17@mail.ru>
2016-11-28 23:33:01 +0900

    does not work

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,12 +1,30 @@
-from django.conf.urls import patterns, include, url
+"""ask URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
 from django.contrib import admin
-admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from qa import views
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    #url(r'^$', include('qa.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^login/', views.test, name = 'test'),
+    url(r'^signup/', views.test, name = 'test'),
+    url(r'^question/.+/', views.test, name = 'test'),
+    url(r'^ask/', views.test, name = 'test'),
+    url(r'^poular/', views.test, name = 'test'),
+    url(r'^new/', views.test, name = 'test'),
+]
--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,9 +1,16 @@
-urlpatterns =  patterns('qa.views',
-  url(r'^$', 'test', name = 'root'),
-  url(r'^login/', 'test'),
-  url(r'^signup/', 'test', name = 'signup'),
-  url(r'^question/\w+/', 'test', name = 'question'),
-  url(r'^ask/', 'test', name = 'ask'),
-  url(r'^popular/', 'test', name = 'popular'),
-  url(r'^new/', 'test', name = 'new'),
-)
+#from blog.views import post_list
+#
+#urlpatterns = patterns('qa.views',
+ #   url(r'^login/','test'),
+#)
+
+from django.conf.urls import url
+
+
+from . import views
+
+from qa.views import test
+
+urlpatterns = [
+    url(r'^login/', test, name='test'),
+]
