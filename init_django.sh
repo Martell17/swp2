c7d967bc9c2fcd65740cc1d74b937e9e841da6b2
Martell17 <martell17@mail.ru>
2016-11-29 11:53:53 +0900

    Update init_django.sh

--- init_django.sh
+++ init_django.sh
@@ -1,1 +1,6 @@
+sudo unlink /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/django.conf
+sudo service nginx restart
 
+cd /home/box/web/ask
+gunicorn ask.wsgi:application --bind 0.0.0.0:8000
